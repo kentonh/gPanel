c3d0aaa7149d43a4593d76570bc929714ebda9f2
Andrew Siegman <blerrnk@gmail.com>
2017-10-28 10:34:04 -0500

    use mime package, update tests

--- pkg/routing/content_types.go
+++ pkg/routing/content_types.go
@@ -3,17 +3,16 @@ package routing
 
 import (
 	"errors"
+	"mime"
 	"path/filepath"
 	"strings"
 )
 
-// getContentType returns the http header safe content-type attribute for the
-// requested path. If the requested path is not formatted correctly with an extension
+// getContentType returns the http header safe Content-Type for the requested
+// path. If the requested path is not formatted correctly with an extension
 // then the function will return the zero-value for a string and an error.
-// BUG(george-e-shaw-iv) Cannot handle interpreted files such as .php files
-// BUG(george-e-shaw-iv) Does not cover the bulk of encountered content types on the web
 func GetContentType(path string) (string, error) {
-	var contentType string
+	var ct string
 
 	path = filepath.Base(path)
 	ext := filepath.Ext(path)
@@ -23,22 +22,11 @@ func GetContentType(path string) (string, error) {
 		return "", errors.New("Invalid file path")
 	}
 
-	switch ext {
-	case ".html":
-		contentType = "text/html"
-	case ".css":
-		contentType = "text/css"
-	case ".js":
-		contentType = "text/javascript"
-	case ".png":
-		contentType = "image/png"
-	case ".jpg":
-		fallthrough
-	case ".jpeg":
-		contentType = "image/jpeg"
-	default:
-		contentType = "text/plain"
+	ct = mime.TypeByExtension(ext)
+
+	if ct == "" {
+		return "", errors.New("Could not get content type")
 	}
 
-	return contentType, nil
+	return ct, nil
 }
